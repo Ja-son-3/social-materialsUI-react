57107f385b8d6d0f42d95e9592d1f24d4e0f0eec
Jason Huang <jason97521@gmail.com>
2023-04-08 07:05:18 -0400

    nav app bar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,64 @@
+import { LogoDev, Mail, Notifications } from '@mui/icons-material'
+import { AppBar, Avatar, Badge, Box, InputBase, Toolbar, Typography, styled } from '@mui/material'
+import React from 'react'
+
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+})
+
+const Search = styled("div")(({theme})=>({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%",
+}))
+
+const Icons = styled(Box)(({theme})=>({
+  display: "none",
+  gap: "20px",
+  alignItems: "center",
+  [theme.breakpoints.up("sm")]:{
+    display: "flex"
+  }
+}))
+
+const UserBox = styled(Box)(({theme})=>({
+  display: "flex",
+  gap: "10px",
+  alignItems: "center",
+  [theme.breakpoints.up("sm")]:{
+    display: "none"
+  }
+}))
+
+
+
+const Navbar = () => {
+  return (
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <Typography variant="h6" sx={{display:{xs:"none", sm:"block"}}}>
+          Social App
+        </Typography>
+        <LogoDev sx={{display:{xs:"block", sm:"none"}}}/>
+        <Search><InputBase placeholder='search...'/></Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={2} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar sx={{width:30, height: 30}} src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
+        </Icons>
+        <UserBox>
+          <Avatar sx={{width:30, height: 30}} src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
+          <Typography variant='span'>John</Typography>
+        </UserBox>
+      </StyledToolbar>
+    </AppBar>
+  )
+}
+
+export default Navbar
